5612dcbc32f5c08f57ae015f6efc2a0ec9087937
DJJS2010 <dylanjamesseymour2010@email.com>
2026-04-01 19:38:36 +0100

    connected discord webhook

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function send(){
+async function send(){
 
 let msg = document.getElementById("msg").value
 
+let webhook = "https://discord.com/api/webhooks/1488970613094617119/4rQwpRgn8CTDOp6bZSUhl0I4Bp6z7Hy_QWpj8zzvaB7OOtT_6kp8hlL2a-UGVM-n5_0m"
+
+await fetch(webhook,{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+content: msg
+})
+})
+
 document.getElementById("status").innerText =
-"Message ready: " + msg
+"Message sent to Discord!"
 
 }
