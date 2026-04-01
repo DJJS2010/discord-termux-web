566d62029a8dedb40d3b91b167783ad69542f692
DJJS2010 <dylanjamesseymour2010@email.com>
2026-04-01 19:43:31 +0100

    added update checker

--- script.js
+++ script.js
@@ -2,19 +2,27 @@ async function send(){
 
 let msg = document.getElementById("msg").value
 
-let webhook = "https://discord.com/api/webhooks/1488970613094617119/4rQwpRgn8CTDOp6bZSUhl0I4Bp6z7Hy_QWpj8zzvaB7OOtT_6kp8hlL2a-UGVM-n5_0m"
-
-await fetch(webhook,{
-method:"POST",
-headers:{
-"Content-Type":"application/json"
-},
-body:JSON.stringify({
-content: msg
-})
-})
-
 document.getElementById("status").innerText =
-"Message sent to Discord!"
+"Message ready: " + msg
 
 }
+
+// show last github update
+async function getUpdate(){
+
+let repo = "DJJS2010/discord-termux-web"
+
+let res = await fetch(
+"https://api.github.com/repos/" + repo + "/commits"
+)
+
+let data = await res.json()
+
+let date = new Date(data[0].commit.author.date)
+
+document.getElementById("update").innerText =
+"Last website update: " + date.toLocaleString()
+
+}
+
+getUpdate()
