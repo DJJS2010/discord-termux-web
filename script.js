1522033e2dc375770746f890d353fd46f4d04376
DJJS2010 <dylanjamesseymour2010@email.com>
2026-04-02 16:33:00 +0100

    send text to my dc server

--- script.js
+++ script.js
@@ -33,3 +33,17 @@ function getUser() {
 }
 
 getUser()
+
+async function send() {
+    let msg = document.getElementById("msg").value;
+
+    const webhook = "https://discord.com/api/webhooks/1488970613094617119/4rQwpRgn8CTDOp6bZSUhl0I4Bp6z7Hy_QWpj8zzvaB7OOtT_6kp8hlL2a-UGVM-n5_0m";
+
+    await fetch(webhook, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ content: msg })
+    });
+
+    document.getElementById("status").innerText = "Message sent to Discord!";
+}
